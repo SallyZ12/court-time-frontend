fbed3bdc5fbb3f36bb1c0294f60cd9ff38711259
SallyZ12 <szasloff@gmail.com>
2019-07-27 14:57:38 -0400

    testing out fetch to check connection between backend and frontend

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/clubs/2')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render () {
     return (
       <div className="App">
-      App
+        App
       </div>
     );
   }

68ae545d61f20e9fa2d5fc91c43266d861cd9a24
SallyZ12 <szasloff@gmail.com>
2019-07-29 18:23:19 -0400

    imported createStore, applyMiddleware, thunk, Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
 import App from './App';
 
 

60509fcab5e0911a56c3d00b7d923306b8c001ca
SallyZ12 <szasloff@gmail.com>
2019-07-29 13:41:22 -0400

    still working on various fetches

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import React from 'react';
 class App extends React.Component {
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/v1/users/1/reservations')
+    fetch('http://localhost:3000/api/v1/clubs/1/reservations')
     .then(response => response.json())
-    .then(data => console.log("user1/reservations",data))
+    .then(data => console.log("clubs/1/reservations:",data[1].user))
   }
 
   render () {

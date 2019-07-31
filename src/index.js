1cc02cf18d4c0aaf4312b0807aa1d437adb515bd
SallyZ12 <szasloff@gmail.com>
2019-07-31 18:38:10 -0400

    wrapped App in Provider, BrowserRouter and defined store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-
 import App from './App';
 
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store.js'
+import { BrowserRouter } from 'react-router-dom'
+
+
+ReactDOM.render(<Provider
+  store={ store }><BrowserRouter><App />
+  </BrowserRouter></Provider>, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));

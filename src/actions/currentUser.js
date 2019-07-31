9d8d7f1b3a421a109c743300a301844299bee20b
SallyZ12 <szasloff@gmail.com>
2019-07-31 18:47:19 -0400

    added functions setCurrentUser and clearCurrentUser to currentUser action file, renamed reducer to currentUserReducer from currentUser.

---
+++ src/actions/currentUser.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+// synchronous action creators
+export const setCurrentUser = user => {
+  return {
+    type: "SET_CURRENT_USER",
+    user
+  }
+}
+
+export const clearCurrentUser = () => {
+  return {
+    type: "CLEAR_CURRENT_USER"
+  }
+}

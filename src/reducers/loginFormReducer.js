2ffcc5ec5b9f0076c84fa8b6ea2f32a7cf428452
SallyZ12 <szasloff@gmail.com>
2019-08-02 10:49:25 -0400

    created file loginFormReducer and added UPDATE_LOGIN_FORM and RESET_LOGIN_FORM cases

---
+++ src/reducers/loginFormReducer.js
@@ -0,0 +1,19 @@
+
+const initialState = {
+   username: "",
+   password: ""
+}
+export default (state = initialState, action) => {
+  switch(action.type) {
+
+    case "UPDATE_LOGIN_FORM":
+      return action.formData
+
+    case "RESET_LOGIN_FORM":
+      return initialState
+
+    default:
+      return state
+
+  }
+}

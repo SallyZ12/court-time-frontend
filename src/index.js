2b9dab4600358f8a41b3829c0757892e4f03f29b
SallyZ12 <szasloff@gmail.com>
2019-08-02 18:19:44 -0400

    reformatted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+// bootstrap imports
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
-
 import { Provider } from 'react-redux'
-
 import store from './store.js'
 import { BrowserRouter } from 'react-router-dom'
 
 
-ReactDOM.render(<Provider
-  store={ store }><BrowserRouter><App />
-  </BrowserRouter></Provider>, document.getElementById('root'));
-
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={ store }>
+                <BrowserRouter>
+                <App />
+                </BrowserRouter>
+                </Provider>, document.getElementById('root'));

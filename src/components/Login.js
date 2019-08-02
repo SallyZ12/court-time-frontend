ef2a548cb7176b5383a4733535ffce2f027e7567
SallyZ12 <szasloff@gmail.com>
2019-08-02 09:39:19 -0400

    src/components/Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+
+class Login extends Component {
+
+  render(){
+    return (
+      <div> This is Login Component </div>
+    )
+  }
+}
+
+export default Login

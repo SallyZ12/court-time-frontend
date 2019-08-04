6f552ee8bc26e9fd972f5f1cbbdcaeb98d1c0083
SallyZ12 <szasloff@gmail.com>
2019-08-04 19:12:59 -0400

    playing with formatting

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Users = ({users}) => {
   // console.log("users:", users)
 return (
+
   <div>
-  <h3> Players List </h3>
+  <h3> Player List </h3>
   {users.users.map(user => <p key={user.id}>
     {user.first_name} {user.last_name},  Admin: {user.admin}</p>)}
     </div>
   )
-
 }
 
 export default Users

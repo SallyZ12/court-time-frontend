08d143e56223962e2ef769030a49014d4a5d3f21
SallyZ12 <szasloff@gmail.com>
2019-08-04 14:21:26 -0400

    got mapping to work by mapping through users.users.map

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,13 +4,11 @@ const Users = ({users}) => {
   console.log("users:", users)
   return (
   <div>
-   Users
+    <p> Name  --------  ADMIN</p>
+  {users.users.map(user => <li key={user.id}>
+    {user.first_name} - {user.last_name} - {user.admin}</li>)}
     </div>
   )
 }
 
 export default Users
-
-
-// {users.map(user => <li key={user.id}>
-//   {user.first_name} </li>)}

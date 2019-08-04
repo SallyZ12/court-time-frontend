a5cc936dda8cde35ad4fe4d681dbb2327130708e
SallyZ12 <szasloff@gmail.com>
2019-08-04 14:09:03 -0400

    new folder to holder container components

---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchUsers} from '../actions/fetchUsers'
+import Users from '../components/Users'
+import UserInput from '../components/UserInput'
+
+class UsersContainer extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchUsers()
+  }
+
+  render() {
+    return (
+      <div>
+      <Users users={this.props.users}/>
+      <UserInput />
+      </div>
+    )
+  }
+}
+
+
+const mapStateToProps = state => {
+  console.log("MSTP:", state.userReducer)
+  return {
+    users: state.userReducer
+  }
+}
+
+
+export default connect(mapStateToProps, {fetchUsers})(UsersContainer)

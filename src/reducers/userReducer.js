a35d1e1c8073719caf288fee2d1a07390f25cbbb
SallyZ12 <szasloff@gmail.com>
2019-08-04 14:33:40 -0400

    fixed so referenced action.payload not action.users

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -5,7 +5,6 @@ export default function userReducer(state = initialState, action) {
   switch (action.type) {
 
     case 'FETCH_USERS':
-    console.log("case:", action.payload)
      return {users: action.payload}
 
     default:

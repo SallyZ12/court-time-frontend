97c0a8cb5cdf178ec0892cdade839a5e0acd82ab
SallyZ12 <szasloff@gmail.com>
2019-08-04 14:53:15 -0400

    changed FETCH_USER case name to DISPLAY_USER and changed action.payload to action.users

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,8 +4,9 @@ const initialState = { users: []}
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
 
-    case 'FETCH_USERS':
-     return {users: action.payload}
+    case 'DISPLAY_USERS':
+    // console.log("reducer:", action.users)
+     return {users: action.users}
 
     default:
       return state

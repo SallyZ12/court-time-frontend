49f22dab2cc6f2973d2c77eb1136f1b553b28fda
SallyZ12 <szasloff@gmail.com>
2019-08-04 18:46:00 -0400

    changed some formatting

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Users = ({users}) => {
   // console.log("users:", users)
-  return (
+return (
   <div>
-    <p> Name  --------  ADMIN</p>
-  {users.users.map(user => <li key={user.id}>
-    {user.first_name} - {user.last_name} - {user.admin}</li>)}
+  <h3> Players List </h3>
+  {users.users.map(user => <p key={user.id}>
+    {user.first_name} {user.last_name},  Admin: {user.admin}</p>)}
     </div>
   )
+
 }
 
 export default Users

a4310a1ddaf26deb612926c0d58847b4e3646f24
SallyZ12 <szasloff@gmail.com>
2019-08-05 10:02:15 -0400

    no real changes, playing with routes and went back to working code

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
+
 const Users = ({users}) => {
-  // console.log("users:", users)
+ console.log("users:", users)
 return (
 
-  <div>
-  <h3> Player List </h3>
-  {users.users.map(user => <p key={user.id}>
-    {user.first_name} {user.last_name},  Admin: {user.admin}</p>)}
-    </div>
-  )
+ <div>
+ <h3> Player List </h3>
+ {users.users.map(user => <p key={user.id}>
+   {user.first_name} {user.last_name},  Admin: {user.admin}</p>)}
+   </div>
+ )
 }
 
 export default Users

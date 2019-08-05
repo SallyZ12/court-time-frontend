64e8b558a66f9436effa4ba5915fbd9b248d15d3
SallyZ12 <szasloff@gmail.com>
2019-08-05 18:16:30 -0400

    created new file

---
+++ src/components/Signup.js
@@ -0,0 +1,6 @@
+
+import React from 'react'
+import {connect} from 'react-redux'
+import {signup} from '../actions/currentUser'
+import {updateSignupForm} from "../actions/signupForm"
+import {resetSignupForm} from "../actions/signupForm"

881d62f91197e4edb8df91cefdd69011ed11da16
SallyZ12 <szasloff@gmail.com>
2019-08-09 14:34:37 -0400

    centered the text, still working on centering the image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import court from '../court.png'
 
+
 const Home = () => {
     return (
-      <div>
 
-      <h2> Welcome to Court Time!</h2>
-      <h4> Your Place To Reserve A Court </h4>
+      <div className = "Home">
+        <h2 className = "text-center"> Welcome to Court Time! </h2>
+        <h4 className = "text-center"> Your Place To Reserve A Court </h4>
       <br/>
       <img src={court} width = '500' height = "250" alt="Court" />;
-      </div>
+    </div>
     )
   }
 

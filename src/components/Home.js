146ebec960b3692d79710d04b468b79c8aab0af6
SallyZ12 <szasloff@gmail.com>
2019-08-10 09:01:05 -0400

    removed a stray ;

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <h2 className = "text-center"> Welcome to Court Time! </h2>
         <h4 className = "text-center"> Your Place To Reserve A Court </h4>
       <br/>
-      <img src={court} width = '500' height = "250" alt="Court" />;
+      <img src={court} width = '500' height = "250" alt="Court" />
     </div>
     )
   }

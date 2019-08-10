d8ed20c9718b70dfd1d219d4d1db854ec450fd63
SallyZ12 <szasloff@gmail.com>
2019-08-10 12:45:54 -0400

    link to players on index page

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 const Users = ({users}) => {
  console.log("users:", users)
@@ -7,10 +7,14 @@ return (
 
  <div>
  <h3> Player List </h3>
- {users.users.map(user => <p key={user.id}>
-   {user.first_name} {user.last_name},  Admin: {user.admin}</p>)}
+ {users.users.map(user => <li><Link to={`/users/$user.id`}>
+   {user.first_name} {user.last_name}</Link></li>)}
    </div>
  )
 }
 
 export default Users
+
+
+// {users.users.map(user => <p key={user.id}>
+//   {user.first_name} {user.last_name},  Admin: {user.admin}</p>)}

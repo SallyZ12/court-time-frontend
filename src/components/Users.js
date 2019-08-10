809c84a04be63ece22ee67b96fc3e01f72253223
SallyZ12 <szasloff@gmail.com>
2019-08-10 16:51:36 -0400

    added Link for a specific user & required code when no user to start

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import User from './User'
 
 const Users = ({users}) => {
  console.log("users:", users)
@@ -7,8 +8,8 @@ return (
 
  <div>
  <h3> Player List </h3>
- {users.users.map(user => <li><Link to={`/users/$user.id`}>
-   {user.first_name} {user.last_name}</Link></li>)}
+ {users.users && users.users.map(user => <li><Link to={`/users/${user.id}`}>
+   {user.first_name} - {user.last_name}</Link></li>)}
    </div>
  )
 }

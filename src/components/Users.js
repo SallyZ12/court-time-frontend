22b11afcb4a3adb957f40030a31743b16c4da278
SallyZ12 <szasloff@gmail.com>
2019-08-11 18:30:05 -0400

    tried using some react-bootstrap styling, didn't work, when back to original list format for now

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,17 +2,17 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 // import User from './User'
 
+
 const Users = ({users}) => {
  // console.log("users:", users)
 return (
 
- <div>
- <h3> Player List </h3>
- <br/>
- {users.users && users.users.map(user => <li key={user.id}> <Link to={`/users/${user.id}`}>
-    {user.first_name} {user.last_name}</Link></li>)}
-   </div>
- )
+  <div>
+  <h3> Player List </h3>
+  <br/>
+    {users.users && users.users.map(user => <li key={user.id}> <Link to={`/users/${user.id}`}>
+      {user.first_name} {user.last_name}</Link></li>)}
+  </div>
+  )
 }
-
 export default Users

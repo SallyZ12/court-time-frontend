f8ab6590f00313763462a91a00ec04b0d0b0907e
SallyZ12 <szasloff@gmail.com>
2019-08-12 17:52:16 -0400

    added current date to Player Information

--- src/components/User.js
+++ src/components/User.js
@@ -2,19 +2,26 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button';
-
+// import moment from 'moment'
+import moment from 'moment'
 
 const User = (props) => {
 // console.log("props:", props )
 
 let user = props.users.users[props.match.params.id-1]
 
+// steps to accessing today's date
+let date = new Date()
+let momentObj = moment(date)
+let currentDate = momentObj.format('MMM DD YYYY')
+
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '35rem' }}>
       {/*<Card.Img variant="top" src="holder.js/100px180" />*/}
       <Card.Body>
-        <Card.Title>Player Information  </Card.Title>
+        <Card.Title> {currentDate} <br/><br/> Player Information </Card.Title>
         <Card.Text>
+        Today's Date: <br/>
         Name: {user ? user.first_name : null} {user ? user.last_name : null} <br/>
         Username: {user ? user.username : null} <br/>
         email: {user ? user.email : null} <br/>
@@ -22,12 +29,11 @@ let user = props.users.users[props.match.params.id-1]
         Reservation Count: {user ? user.reservations.length : null}<br/><br/>
         Reservations <br/>
           {user && user.reservations.map(reservation => <li key={reservation.id}>
-             {reservation.day} - {reservation.hour} - ${reservation.rate}</li>)}
+             {reservation.day} - {reservation.hour} - ${reservation.rate} - ConfirmID: {reservation.confirmID}</li>)}
         </Card.Text>
         <Button variant="light"><Link to='/users'>Players</Link></Button>
       </Card.Body>
     </Card>
-
   )
 }
 

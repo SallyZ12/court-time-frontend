5cf924a69baa275bf1817102eb9fc7972314366e
SallyZ12 <szasloff@gmail.com>
2019-08-13 12:12:30 -0400

    fixed some formatting

--- src/components/Users.js
+++ src/components/Users.js
@@ -9,7 +9,7 @@ const Users = ({users}) => {
 return (
 
 
-  <Table striped bordered hover size="sm">
+  <Table responsive>
     <thead>
       <tr>
         <th>Name</th>
@@ -17,45 +17,11 @@ return (
     </thead>
       <tbody>
     <tr>
-      <td> {users.users && users.users.map(user => (
-      <Link to={`/users/${user.id}`}> <li> {user.first_name} {user.last_name} </li></Link>))} </td>      
+      <td> {users.users && users.users.map(user => <> <Link key={user.id}
+      to={`/users/${user.id}`}> {user.first_name} {user.last_name}</Link> <br/> </>)} </td>
     </tr>
     </tbody>
     </Table>
   )
 }
 export default Users
-
-
-// <Table striped bordered hover size="sm">
-//   <thead>
-//     <tr>
-//       <th>#</th>
-//       <th>First Name</th>
-//       <th>Last Name</th>
-//       <th>Username</th>
-//     </tr>
-//   </thead>
-//   <tbody>
-//     <tr>
-//       <td>1</td>
-//       <td>Mark</td>
-//       <td>Otto</td>
-//       <td>@mdo</td>
-//     </tr>
-//     <tr>
-//       <td>2</td>
-//       <td>Jacob</td>
-//       <td>Thornton</td>
-//       <td>@fat</td>
-//     </tr>
-//     <tr>
-//       <td>3</td>
-//       <td colSpan="2">Larry the Bird</td>
-//       <td>@twitter</td>
-//     </tr>
-//   </tbody>
-// </Table>
-
-// <Link to={`/users/${user.id}`}>
-//   {user.first_name} {user.last_name}</Link></ul>)}

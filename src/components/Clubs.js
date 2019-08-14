2510b1585583955a618ecc83e277f10950ce5efe
SallyZ12 <szasloff@gmail.com>
2019-08-14 13:53:05 -0400

    added code to display Clubs

--- src/components/Clubs.js
+++ src/components/Clubs.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const Clubs = () => {
-
+const Clubs = ({clubs}) => {
+// console.log("clubs", clubs)
 return (
 
   <div>
-  Clubs
+
+  {clubs.clubs && clubs.clubs.map(club =>
+    <li key={club.id}>
+    <Link to={`/clubs/${club}`}> {club.club_name} - {club.location} </Link>
+    </li>)}
   </div>
-)
+  )
 }
 
 export default Clubs

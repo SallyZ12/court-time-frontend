9bbead26b2b87836b2addec7507eb465d6ae2d19
SallyZ12 <szasloff@gmail.com>
2019-08-14 13:06:26 -0400

    passing through ClubsContainer to start set-up of Clubs/Courts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import court from '../court.png'
 import '../style.css';
+import ClubsContainer from '../containers/ClubsContainer'
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <div className = "Home">
         <h2 className = "text-center"> Welcome to Court Time! </h2>
         <h4 className = "text-center"> Your Place To Reserve A Court </h4>
+        <ClubsContainer/>
       <br/>
       <img src={court} width = '500' height = "250" alt="Court" className="center" />
     </div>

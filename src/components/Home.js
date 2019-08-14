cc8630eb6358075f042b0b239bf8b40d8e0ea85b
SallyZ12 <szasloff@gmail.com>
2019-08-14 14:01:50 -0400

    added text for selection of Clubs

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ const Home = () => {
       <div className = "Home">
         <h2 className = "text-center"> Welcome to Court Time! </h2>
         <h4 className = "text-center"> Your Place To Reserve A Court </h4>
+        <h4 className = "text-center"> Please select a Club </h4>
         <ClubsContainer/>
       <br/>
       <img src={court} width = '500' height = "250" alt="Court" className="center" />

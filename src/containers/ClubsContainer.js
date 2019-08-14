cf33ce8d8cfb32ab3b3ab70bea0efc7a57864850
SallyZ12 <szasloff@gmail.com>
2019-08-14 13:54:11 -0400

    passed through clubs from Clubs

--- src/containers/ClubsContainer.js
+++ src/containers/ClubsContainer.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchClubs} from '../actions/fetchClubs'
 import Clubs from '../components/Clubs'
 
 
 class ClubsContainer extends React.Component {
 
   componentDidMount() {
-
+    this.props.fetchClubs()
   }
 
   render() {
     return (
       <div>
-      <Clubs/>
+      <Clubs clubs={this.props.clubs}/>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
+  console.log("state:", state.clubsReducer)
   return {
     clubs: state.clubsReducer
   }
 }
 
-export default connect(mapStateToProps)(ClubsContainer)
+export default connect(mapStateToProps, {fetchClubs})(ClubsContainer)

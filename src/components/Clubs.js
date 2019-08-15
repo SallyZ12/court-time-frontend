cd0760508879f5eb1e667d2eee4816223f67babb
SallyZ12 <szasloff@gmail.com>
2019-08-15 13:50:04 -0400

    trying to limit Register/Login to Clubs index page--in process

--- src/components/Clubs.js
+++ src/components/Clubs.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import '../style.css';
+// import Button from 'react-bootstrap/Button';
+
 
 const Clubs = ({clubs}) => {
 // console.log("clubs", clubs)

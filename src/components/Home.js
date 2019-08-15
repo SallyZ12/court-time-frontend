ac53e5f0b2802c966e14258c0e883052fc7a7bb7
SallyZ12 <szasloff@gmail.com>
2019-08-15 15:42:51 -0400

    changed import to Clubs from ClubsContainer, passed props into Home function, rendered Clubs component passing in clubs props {props.home.clubs}

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import court from '../court.png'
 import '../style.css';
-import ClubsContainer from '../containers/ClubsContainer'
+import Clubs from '../components/Clubs'
 
-const Home = () => {
+const Home = (props) => {
+  console.log("home:", props)
     return (
 
       <div className = "Home">
         <h2 className = "text-center"> Welcome to Court Time! </h2>
         <h4 className = "text-center"> Your Place To Reserve A Court </h4>
         <h4 className = "text-center"> Please select a Club </h4>
-      <ClubsContainer/>
+      <Clubs clubs = {props.home.clubs}/>
       <br/>
       <img src={court} width = '500' height = "250" alt="Court" className="center" />
     </div>

89c181e1862b73225cf18793d44a566c247db238
SallyZ12 <szasloff@gmail.com>
2019-08-17 20:23:00 -0400

    tried a different format, did not work

--- src/components/Club.js
+++ src/components/Club.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import {Link} from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
-
+import '../style.css'
 // import Button from 'react-bootstrap/Button';
 
 
@@ -10,10 +10,15 @@ const Club = (props) => {
 
 let club = props.clubs.clubs[props.match.params.id-1]
 
+// {court_surface} === "grass" green
+// {court_surface} === "hard"  blue
+// {court_surface} === "har-tru" grey
+// {court_surface} === "clay" orange
+
 return (
 
 
-  <Card style={{ width: '35rem' }}>
+  <Card style={{ width: '35rem' }} >
     <Card.Body>
       <p> Please Register or Login to Reserve a Court</p>
       <Card.Title> Club:   {club ? club.club_name : null} </Card.Title>
@@ -31,6 +36,9 @@ return (
   )
 }
 export default Club;
+
+
+
 
   // Button/Link to Clubs removed since Clubs now available only from Home component.
   // <Button variant="light"><Link to='/clubs'>Clubs</Link></Button>

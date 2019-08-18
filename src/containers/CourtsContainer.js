8beea747df431073b9af81cb0d513e647cd2e933
SallyZ12 <szasloff@gmail.com>
2019-08-18 15:37:45 -0400

    removed passing in courts props from this container since this is rendered in the Club component

--- src/containers/CourtsContainer.js
+++ src/containers/CourtsContainer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import CourtInput from '../components/CourtInput'
-import Courts from '../components/Courts'
+
 
 class CourtsContainer extends React.Component {
   render () {
     return (
     <div>
     <CourtInput club={this.props.club}/>
-    <Courts courts = {this.props.club && this.props.club.courts}/>
+
     </div>
   )
   }

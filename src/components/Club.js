fc922feff6d75c5c92c020e20fb926631fb65f96
SallyZ12 <szasloff@gmail.com>
2019-08-20 12:45:46 -0400

    formatting changes

--- src/components/Club.js
+++ src/components/Club.js
@@ -4,6 +4,9 @@ import Card from 'react-bootstrap/Card'
 import '../style.css'
 import Button from 'react-bootstrap/Button';
 import { NavLink } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Club = (props) => {
 // console.log("props:", props )
@@ -13,9 +16,25 @@ let club = props.clubs.clubs[props.match.params.id-1]
 // console.log("club:", club)
 
 
+let surface = club && club.courts.filter(court => court.surface )
+console.log(surface)
+
+
 return (
 <div>
-  <Button variant="light"><NavLink exact activeClassName="current" to={ club && `/clubs/${club.id}/courts`}> New Court</NavLink></Button>
+
+<Container>
+  <Row >
+  <Col> Surface </Col>
+  <Col> Prime </Col>
+  <Col> Non-Prime </Col>
+  </Row>
+{club && club.courts.map(court => <span key={court.id}>
+  <Row >
+  <Col> {court.surface} </Col>
+  <Col> {court.prime}</Col>
+  <Col> {court.non_prime}</Col>
+    </Row></span>)}
   <Card style={{ width: '45rem' }} >
     <Card.Body>
 
@@ -23,12 +42,13 @@ return (
         <Card.Text className="courts">
          <br/>
           {club && club.courts.map(court => <li key={court.id}>
-            Court: {court.court_number} <br/> Surface: {court.surface} <br/> Prime: ${court.prime} <br/>   Non-Prime: ${court.non_prime} <br/><br/></li> )}
+            Court: {court.court_number} <br/>{court.surface} <br/><br/></li> )}
             <br/><br/>
         </Card.Text>
     </Card.Body>
   </Card>
-
+    <Button variant="light"><NavLink exact activeClassName="current" to={ club && `/clubs/${club.id}/courts`}> New Court</NavLink></Button>
+</Container>
   </div>
   )
 }

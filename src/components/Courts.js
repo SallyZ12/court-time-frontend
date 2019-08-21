5f7a3e1d674d33fe69e1db05b10d027cb956ce37
SallyZ12 <szasloff@gmail.com>
2019-08-21 18:14:45 -0400

    some format changes, but not using this component

--- src/components/Courts.js
+++ src/components/Courts.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {deleteCourt} from '../actions/deleteCourt'
+import Button from 'react-bootstrap/Button';
 
-const Courts = (props) => {
+const Courts = ({club}) => {
+
+console.log("courts component:", club)
+
+const handleDelete = (court) => {
+  club.deleteCourt(court.id, court.club_id)
+}
+
   return (
     <div>
-    {props.courts && props.courts.map(court =>
+    {club.club.courts && club.club.courts.map(court =>
     <li key={court.id}>
-    {court.court_number} - {court.surface} - {court.prime} - {court.non_prime}</li>
+    {court.court_number} - {court.surface} - {court.prime} - {court.non_prime}<br/>
+    <Button onClick={()=> handleDelete(court)}> Delete </Button><br/></li>
   )}
   </div>
   )
 }
 
-export default Courts
+export default connect(null, {deleteCourt})(Courts)

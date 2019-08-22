fc4353c38b3261fae1596c81bf2c88badd0580c5
SallyZ12 <szasloff@gmail.com>
2019-08-22 13:28:37 -0400

    new file to start process of reservations

---
+++ src/components/Reservation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {connect} from 'react-redux'
+
+const Reservation = (props) => {
+
+return (
+  <div>
+  Reservation
+  </div>
+)
+}
+
+export default connect(null)(Reservation)

2dea10fc8c86b9612a8704bde946cea95377ed6d
SallyZ12 <szasloff@gmail.com>
2019-08-25 17:38:20 -0400

    working out inputs for Reservation's

--- src/components/ReservationInput.js
+++ src/components/ReservationInput.js
@@ -2,14 +2,33 @@ import React from 'react'
 import{connect} from 'react-redux'
 import uuid from 'uuid'
 import moment from 'moment'
+import { withRouter } from 'react-router-dom';
+// import Form from 'react-bootstrap/Form';
+// import Col from 'react-bootstrap/Col';
+
 
 class ReservationInput extends React.Component {
 
-  state = {
+
+constructor() {
+  super()
+
+  this.currentDate = new Date()
+  this.dayOne = moment(this.currentDate).add(1, 'days').format('MMM DD YYYY')
+  this.dayTwo = moment(this.currentDate).add(2, 'days').format('MMM DD YYYY')
+  this.dayThree = moment(this.currentDate).add(3, 'days').format('MMM DD YYYY')
+  this.dayFour = moment(this.currentDate).add(4, 'days').format('MMM DD YYYY')
+  this.dayFive = moment(this.currentDate).add(5, 'days').format('MMM DD YYYY')
+  this.daySix = moment(this.currentDate).add(6, 'days').format('MMM DD YYYY')
+  this.daySeven = moment(this.currentDate).add(7, 'days').format('MMM DD YYYY')
+
+  this.state = {
     day: "",
+    hour: "",
     rate: "",
     confirmId: ""
   }
+}
 
   handleChange = (event) => {
     this.setState({
@@ -19,10 +38,11 @@ class ReservationInput extends React.Component {
 
   handleSubmit = (event)=> {
     event.preventDefault()
-    const reservation = {this.state, confirmId: uuid()}
+    
     this.setState({
 
       day: "",
+      hour: "",
       rate: "",
       confirmId: ""
     })
@@ -33,17 +53,37 @@ class ReservationInput extends React.Component {
     return (
       <div>
       <h5> Reservation </h5>
-      <Form>
-        <Form.group as={Col}>
-        <input type= 'date'
+      <form>
+        <select
+          name = 'day'
+          value = {this.state.day}
+          onChange = {this.handleChange}>
+          <option value = "{this.dayOne}"> {this.dayOne} </option>
+          <option value = "{this.dayTwo}"> {this.dayTwo} </option>
+          <option value = "{this.dayThree}"> {this.dayThree} </option>
+          <option value = "{this.dayFour}"> {this.dayFour} </option>
+          <option value = "{this.dayFive}"> {this.dayFive} </option>
+          <option value = "{this.daySix}"> {this.daySix} </option>
+          <option value = "{this.daySeven}"> {this.daySeven} </option>
+          </select>
+          <br/>
+        <select
+          name = 'hour'
+          value = {this.state.hour}
+          onChange = {this.handleChange}>
+            <option value = "9:00am">9:00 am </option>
+            <option value = "10:00am">10:00 am </option>
+            <option value = "11:00am">11:00 am </option>
+            <option value = "12:00pm">12:00 pm</option>
+            <option value = "1:00pm">1:00 pm </option>
+            <option value = "2:00pm">2:00 pm </option>
+          </select>
 
-
+      </form>
       </div>
-
-
 
     )
   }
 }
 
-export default ReservationInput
+export default connect(ReservationInput)

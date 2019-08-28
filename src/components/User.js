dd408da117c56c35846a511772b14eb4241598e8
SallyZ12 <szasloff@gmail.com>
2019-08-28 13:54:20 -0400

    deleted render of court_rate and club

--- src/components/User.js
+++ src/components/User.js
@@ -36,7 +36,7 @@ let currentDate = moment(date).format('MMM DD YYYY')
         Reservation Count: {user ? user.reservations.length : null}<br/><br/>
         Reservations <br/>
           {user && user.reservations.map(reservation => <li key={reservation.id}>
-             ConfirmID: {reservation.confirmID} - {reservation.day} - {reservation.hour} - ${reservation.rate} - {club && club.club_name}</li>)}
+             ConfirmID: {reservation.confirmID} - {reservation.day} - {reservation.hour}  </li>)}
         </Card.Text>
         <Button variant="light"><Link to='/users'>Players</Link></Button>
       </Card.Body>

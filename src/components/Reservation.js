8300005a1c6975fbae3f0d46fe83a44341bb8a08
SallyZ12 <szasloff@gmail.com>
2019-09-03 12:07:25 -0400

    some formatting

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -3,10 +3,13 @@ import {connect} from 'react-redux'
 import moment from 'moment'
 
 const Reservation = (props) => {
-console.log("reservation:", props)
+// console.log("reservation:", props)
 
-  let reservation =  props.currentUser && props.currentUser.reservations[props.match.params.id-1]
-  
+  // let reservation =  props.currentUser && props.currentUser.reservations[props.match.params.id-1]
+
+  // this will allow you to find a reservation through a link where a reservation is deleted or the
+  // sequence of reservation.ids is not in order since various currentUsers will book reservations.
+  let reservation = props.currentUser && props.currentUser.reservations.filter(reservation => reservation.id == props.match.params.id)[0]
 
 
 return (

6ee654cbc109c78e0fe8502213a15498c4b1654f
SallyZ12 <szasloff@gmail.com>
2019-09-04 13:38:55 -0400

    added a NavLink for a currentUser's reservations

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
+import {NavLink} from 'react-router-dom'
 
 const NavBar =  ({ currentUser}) => {
 // console.log("currentUser:", currentUser)
   return (
     <div className="NavBar">
        {currentUser ? <h4> Welcome, {currentUser.first_name} </h4> : ""}
+       {currentUser ?  <p> <NavLink exact to={`users/${currentUser.id}`}>Reservations</NavLink></p> : ""}
     </div>
   )
 }

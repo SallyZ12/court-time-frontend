7c7f98b766a55ee6a0084448de06839b284af0ed
SallyZ12 <szasloff@gmail.com>
2019-09-04 13:40:12 -0400

    commented out import of Button since commented out button for players

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import moment from 'moment'
 
 

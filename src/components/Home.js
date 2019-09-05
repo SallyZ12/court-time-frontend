5a572304d17993607f6e5b3600c824da1ec81307
SallyZ12 <szasloff@gmail.com>
2019-09-05 17:08:29 -0400

    tried an alternative link rather than using NavBar, but went back to NavBar so no changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,13 @@ import '../style.css';
 import Clubs from '../components/Clubs'
 
 
+
 const Home = (props) => {
   // console.log("home:", props)
     return (
 
       <div className = "Home">
+
         <h2 className = "text-center"> It's Court Time! </h2>
         <h6 className = "text-center"> Register/Login To Begin </h6>
         <h4 className = "text-center"> Select a Club </h4>

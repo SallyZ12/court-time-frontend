efaeb94a9dbeae05e5f295cd0fdcf0fc951ffb5a
SallyZ12 <szasloff@gmail.com>
2019-09-06 15:56:27 -0400

    added a case for deleting a reservation from current User

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -5,13 +5,14 @@ export default (state = {}, action) => {
 
     case "SET_CURRENT_USER":
     case "ADD_RESERVATION_TO_CURRENT_USER":
+    case "DELETE_RESERVATION_FROM_CURRENT_USER":
       return action.user
 
     case "CLEAR_CURRENT_USER":
       return {}
 
-
     default:
       return state
-  }
+
+    }
 }

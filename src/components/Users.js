2dcc175580e5911242781ce4c6c6d241b2768ac5
SallyZ12 <szasloff@gmail.com>
2019-09-09 15:33:39 -0400

    added sort function to sort by first_name

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,11 +5,23 @@ import {Link} from 'react-router-dom'
 const Users = ({users}) => {
  // console.log("users:", users)
 
+function sortByName(data) {
+  return data.sort(function(a,b) {
+    let nameA = a.first_name
+      let nameB = b.first_name
+      let nameSort = nameA.localeCompare(nameB)
+        if(nameSort !== 0){
+        return nameSort
+        }
+  })
+}
+
+
 return (
 
   <div>
     <h5> Players - Count: {users.users.length} </h5>
-     {users.users && users.users.map(user =>
+     {users.users && sortByName(users.users).map(user =>
       <p key={user.id}>
       <Link to={`/users/${user.id}`}> {user.first_name} {user.last_name}</Link>
     </p>)}

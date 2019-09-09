dc75ca7e6caf52d52d560bd64d9395b3b83d46a9
SallyZ12 <szasloff@gmail.com>
2019-09-09 09:02:03 -0400

    added route for ClubInput

--- src/containers/ClubsContainer.js
+++ src/containers/ClubsContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchClubs} from '../actions/fetchClubs'
-
+import ClubInput from '../components/ClubInput'
 import Club from '../components/Club'
 import { Route, Switch } from 'react-router-dom'
 
@@ -16,6 +16,7 @@ class ClubsContainer extends React.Component {
       <div>
       <Switch>
         <Route path="/clubs/:id" render={(routerProps)=> <Club {...routerProps} clubs={this.props.clubs} currentUser={this.props.currentUser}/>}/>
+        <Route exact path = '/clubs' render ={() => <ClubInput clubInput = {this.props.club}/>} />
       </Switch>
 
       </div>

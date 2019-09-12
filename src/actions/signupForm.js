54b584eb8c8676118bea756d153af0740319c74c
SallyZ12 <szasloff@gmail.com>
2019-09-12 13:55:41 -0400

    added id to userFormData

--- src/actions/signupForm.js
+++ src/actions/signupForm.js
@@ -23,6 +23,7 @@ export const editSignupForm = currentUser => {
 
 export const setFormDataForEdit = user => {
   const userFormData = {
+    id: user.id,
     first_name: user.first_name,
     last_name: user.last_name,
     email: user.email,

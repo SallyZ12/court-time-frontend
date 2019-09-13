715cc7df2b658b11fb994a38b49b4ce76e2050c4
SallyZ12 <szasloff@gmail.com>
2019-09-13 14:18:55 -0400

    fixed Edit User case, needed to make user a key and then surround with curly brackets to make an object

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -24,8 +24,7 @@ export default function usersReducer(state = initialState, action) {
 
 // work in progress
       case 'EDIT_USER':
-    
-        return state.users.map(user => user.id === action.user.id ? action.user : user && user)
+        return {users: state.users.map(user => user.id === action.user.id ? action.user : user && user)}
 
     default:
       return state

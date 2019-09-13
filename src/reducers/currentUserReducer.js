59046027fa1eb572377b3f955c7c4b28064bcb5b
SallyZ12 <szasloff@gmail.com>
2019-09-13 13:42:48 -0400

    consolidated Add Edited User to Current User by stacking the case

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -6,16 +6,14 @@ export default (state = {}, action) => {
     case "SET_CURRENT_USER":
     case "ADD_RESERVATION_TO_CURRENT_USER":
     case "DELETE_RESERVATION_FROM_CURRENT_USER":
+    case "ADD_EDITED_USER_TO_CURRENT_USER":
       return action.user
 
     case "CLEAR_CURRENT_USER":
       return {}
 
     case "SET_FORM_DATA_FOR_EDIT":
-     return action.userFormData
-
-       case "ADD_EDITED_USER_TO_CURRENT_USER":
-        return action.user
+     return action.userFormData       
 
     default:
       return state

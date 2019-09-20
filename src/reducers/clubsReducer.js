ded1b58391816095a530802521fdab86606f28dc
SallyZ12 <szasloff@gmail.com>
2019-09-20 10:39:32 -0400

    fixed the case ADD_RESERVATION_TO_COURT by grabbing the new reservation object by its id rather than locating by the index in the array

--- src/reducers/clubsReducer.js
+++ src/reducers/clubsReducer.js
@@ -26,14 +26,15 @@ export default function clubsReducer(state = initialState, action) {
 
 
     case 'ADD_RESERVATION_TO_COURT':
-let maxResId = (Math.max(...action.payload.reservations.map(reservation => reservation.id)))
-    let clubs2 = state.clubs.map(club => {
-      	if (club.club_name === action.payload.reservations[action.payload.reservations.length-1].reservation_club) {
-              let court = club.courts.find(court => court.id === action.payload.reservations[action.payload.reservations.length-1].court_id)
-      		court.court_res.push(action.payload.reservations.find(reservation => reservation.id === maxResId))}
-      	return club
-      })
-      return {...state, clubs: clubs2}
+      let maxResId = (Math.max(...action.payload.reservations.map(reservation => reservation.id)))
+
+           let clubs2 = state.clubs.map(club => {
+      	        if (club.club_name === (action.payload.reservations.find(reservation => reservation.id === maxResId)).reservation_club) {
+                   let court = club.courts.find(court => court.id === (action.payload.reservations.find(reservation => reservation.id === maxResId)).court_id)
+      		           court.court_res.push(action.payload.reservations.find(reservation => reservation.id === maxResId))}
+      	        return club
+           })
+            return {...state, clubs: clubs2}
 
 
     default:

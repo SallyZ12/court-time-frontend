498629addf58c9f348bb3b5c52c23e717f265fac
SallyZ12 <szasloff@gmail.com>
2019-09-25 10:43:59 -0400

    added clubs MSTP and passed in clubs in ReservationInput route

--- src/containers/ReservationsContainer.js
+++ src/containers/ReservationsContainer.js
@@ -10,7 +10,7 @@ class ReservationsContainer extends React.Component {
   render() {
     return (
       <div>
-      <Route exact path="/users/:id/reservations" render ={() => <ReservationInput currentUser = {this.props.currentUser}/>}/>
+      <Route exact path="/users/:id/reservations" render ={() => <ReservationInput currentUser = {this.props.currentUser} clubs={this.props.clubs}/>}/>
       <Route exact path="/reservations/:id" render = {(routerProps) => <Reservation {...routerProps} currentUser = {this.props.currentUser}/>}/>
       </div>
     )
@@ -19,7 +19,8 @@ class ReservationsContainer extends React.Component {
 
 const mapStateToProps = state => {
   return ({
-  currentUser: state.currentUserReducer
+  currentUser: state.currentUserReducer,
+  clubs: state.clubsReducer
 })
 }
 

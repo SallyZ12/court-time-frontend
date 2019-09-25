e1f870ff49d581c2df13b86401f3939abec27a77
SallyZ12 <szasloff@gmail.com>
2019-09-25 10:49:35 -0400

    removed clubs in MSTP and clubs in render for ReservationInput

--- src/containers/ReservationsContainer.js
+++ src/containers/ReservationsContainer.js
@@ -10,7 +10,7 @@ class ReservationsContainer extends React.Component {
   render() {
     return (
       <div>
-      <Route exact path="/users/:id/reservations" render ={() => <ReservationInput currentUser = {this.props.currentUser} clubs={this.props.clubs}/>}/>
+      <Route exact path="/users/:id/reservations" render ={() => <ReservationInput currentUser = {this.props.currentUser} />}/>
       <Route exact path="/reservations/:id" render = {(routerProps) => <Reservation {...routerProps} currentUser = {this.props.currentUser}/>}/>
       </div>
     )
@@ -19,8 +19,7 @@ class ReservationsContainer extends React.Component {
 
 const mapStateToProps = state => {
   return ({
-  currentUser: state.currentUserReducer,
-  clubs: state.clubsReducer
+  currentUser: state.currentUserReducer  
 })
 }
 

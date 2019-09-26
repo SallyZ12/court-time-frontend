f350eb88646bd47687c35209bae71a57622d151f
SallyZ12 <szasloff@gmail.com>
2019-09-26 12:36:25 -0400

    added new file for the array of objects for reservation times in dropdown menu for ReservationInput

---
+++ src/data.js
@@ -0,0 +1,8 @@
+
+export const time = [{id: 0, value: 'Hour', label: 'time'},
+             {id: 1, value:'9:00 am',label:'9:00 am'},
+             {id: 2, value:'10:00 am',label:'10:00 am'},
+             {id: 3, value:'11:00 am',label:'11:00 am'},
+             {id: 4, value:'12:00 pm',label:'12:00 pm'},
+             {id: 5, value:'1:00 pm',label:'1:00 pm'},
+             {id: 6, value:'2:00 pm',label:'2:00 pm'}]

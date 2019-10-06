56fcca077f08c9439d92270d66d1b42ca9378345
SallyZ12 <szasloff@gmail.com>
2019-10-06 11:23:40 -0400

    commented out entire component since not using

--- src/components/Courts.js
+++ src/components/Courts.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {deleteCourt} from '../actions/deleteCourt'
-import Button from 'react-bootstrap/Button';
+// import React from 'react'
+// import {connect} from 'react-redux'
+// import {deleteCourt} from '../actions/deleteCourt'
+// import Button from 'react-bootstrap/Button';
+//
+// const Courts = ({club}) => {
+//
+// const handleDelete = (court) => {
+//   club.deleteCourt(court.id, court.club_id)
+// }
 
-const Courts = ({club}) => {
-
-const handleDelete = (court) => {
-  club.deleteCourt(court.id, court.club_id)
-}
-
-  return (
-    <div>
+  // return (
+  {/*  <div>
     {club.club.courts && club.club.courts.map(court =>
     <li key={court.id}>
     {court.court_number} - {court.surface} - {court.prime} - {court.non_prime}<br/>
     <Button onClick={()=> handleDelete(court)}> Delete </Button><br/></li>
   )}
-  </div>
+  </div>*/}
   )
 }
 
-export default connect(null, {deleteCourt})(Courts)
+// export default connect(null, {deleteCourt})(Courts)

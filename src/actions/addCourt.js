5e7e82e97c08b5bc35b994739af4550dd713e576
SallyZ12 <szasloff@gmail.com>
2019-10-08 14:18:10 -0400

    added sendable data to convert front end courtNumber to backend court_number and nonPrime to non_prime

--- src/actions/addCourt.js
+++ src/actions/addCourt.js
@@ -2,20 +2,29 @@
 export const addCourt = (court, clubId) => {
 
   return (dispatch) => {
+
+    const sendableCourtData ={
+      court_number: court.courtNumber,
+      club_id: court.club_id,
+      surface: court.surface,
+      prime: court.prime,
+      non_prime: court.nonPrime
+    }
+
     fetch (`http://localhost:3000/api/v1/clubs/${clubId}/courts`, {
       method: 'POST',
       credentials: "include",
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(court)
+      body: JSON.stringify(sendableCourtData)
     })
       .then(response => response.json())
       .then(club => {
         if (club.error) {
           alert(club.error)
         } else {
-        
+
           dispatch({type: 'ADD_COURT', payload: club})
         }
     })

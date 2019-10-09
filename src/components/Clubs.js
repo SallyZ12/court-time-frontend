4c8e47dbf8a7a104998f027c968bc4feb44db5ee
SallyZ12 <szasloff@gmail.com>
2019-10-09 17:08:26 -0400

    added functionality to vote for each tennis club

--- src/components/Clubs.js
+++ src/components/Clubs.js
@@ -4,49 +4,29 @@ import {Link} from 'react-router-dom'
 
 class Clubs extends React.Component {
 
-    state= {
-      vote: 0
+  constructor(){
+    super()
+    this.state = {}
+  }
+
+    handleClick = (clubId) => {
+      this.state[clubId] ? this.setState({[clubId]: this.state[clubId] + 1}) : this.setState({[clubId]: 1})
     }
-
-    handleClick = (event) => {
-      this.setState(prevState => ({
-          vote: prevState.vote + 1
-      }))}
-
 
 
   render(){
 
     const {clubs} = this.props
 
-
     return (
-
       <div>
-
-
       {clubs && clubs.map(club =>
         <p className ='text-center' key={club.id}>
-
-        <Link to={`/clubs/${club.id}`}> {club.clubName} - {club.location} - {club.courts.length} Courts </Link>   <button onClick = {this.handleClick} >Vote {this.state.vote} </button><br/>
+        <Link to={`/clubs/${club.id}`}> {club.club_name} - {club.location} - {club.courts.length} Courts </Link>   <button  onClick = {(event) => this.handleClick(club.id)}  >Vote {this.state[club.id]}  </button><br/>
         </p>)}
       </div>
       )
     }
   }
-
-
-
-
-
-
-
-
-
-// console.log("clubs", clubs)
-
-
-
-
 
 export default Clubs

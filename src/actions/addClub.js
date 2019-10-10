4213731f5e41a0f0d6a90d31805197d962216616
SallyZ12 <szasloff@gmail.com>
2019-10-09 21:36:12 -0400

    added const sendableClubData to convert from end data in JS format to backend ruby syntax

--- src/actions/addClub.js
+++ src/actions/addClub.js
@@ -2,13 +2,18 @@
 //asynchronous action creators
 export const addClub = (clubData, history) => {
 
+  const sendableClubData ={
+        club_name: clubData.clubName,
+        location: clubData.location
+      }
+
   return (dispatch) => {
     fetch ('http://localhost:3000/api/v1/clubs', {
       headers: {
         'Content-Type': 'application/json',
       },
       method: 'POST',
-      body: JSON.stringify(clubData)
+      body: JSON.stringify(sendableClubData)
     })
       .then(response => response.json())
       .then(club => {

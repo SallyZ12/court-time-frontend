ccc872b97a5c07a7bce3776793891fb6dee7f4ce
SallyZ12 <szasloff@gmail.com>
2019-12-11 14:14:54 -0500

    some formatting

--- src/components/Club.js
+++ src/components/Club.js
@@ -37,7 +37,7 @@ const handleDeleteCourt = (court) => {
 
 const handleDeleteClub = (club) => {
   props.deleteClub(club.id)
-  props.history.push("/")
+  // props.history.push("/")
 }
 
 function sortByDate(data) {

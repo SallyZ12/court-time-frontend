c4388eb4490b18d6d3491a62e29e30d44601890a
SallyZ12 <szasloff@gmail.com>
2019-12-12 12:39:10 -0500

    new file to start process of editing club data

---
+++ src/components/NewClubFormWrapper.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ClubInput from '../components'
+import { addClub } from '...actions/addClub'
+import { connect } from 'react-redux'
+
+const NewClubFormWrapper = ( {history, addClub}) => {
+
+  const handleSubmit = {clubData, clubId} => {
+
+    addClub({
+      ...clubData,
+      clubId
+    }, history)
+  }
+
+  return <ClubInput history={history} handleSubmit={handleSubmit} />
+}
+
+export default connect(null, { addClub})(NewClubFormWrapper)

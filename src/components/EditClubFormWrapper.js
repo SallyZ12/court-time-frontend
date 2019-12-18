78a04edaf507e25c6f75f01a912502e7517334e4
SallyZ12 <szasloff@gmail.com>
2019-12-18 14:41:24 -0500

    building component to edit Club info

---
+++ src/components/EditClubFormWrapper.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import ClubInput from '../components/ClubInput'
+import { editClub,setClubFormDataForEdit } from '../actions/addClub'
+
+class EditClubFormWrapper extends React.Component {
+
+  componentDidMount() {
+    this.props.club && this.props.setClubFormDataForEdit(this.props.club)
+  }
+
+  componentDidUpdate(prevProps) {
+    this.props.club && !prevProps.club && this.props.setClubFormDataForEdit(this.props.club)
+  }
+
+
+  handleSubmit = (clubFormData) => {
+    const { editClub, club, history } = this.props
+      editClub({
+        ...clubFormData,
+        clubId: club.id
+      }, history)
+  }
+
+  render() {
+    return <>
+      <ClubInput editMode handleSubmit={this.handleSubmit} />
+      <br/>
+      </>
+    }
+}
+
+export default connect(null, {editClub, setClubFormDataForEdit})(EditClubFormWrapper)

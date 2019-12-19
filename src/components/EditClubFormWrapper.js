15d631514aa7efd1e76fd775281b88e2b636683b
SallyZ12 <szasloff@gmail.com>
2019-12-19 16:31:37 -0500

    added import connect

--- src/components/EditClubFormWrapper.js
+++ src/components/EditClubFormWrapper.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {connect} from 'react-redux'
 import ClubInput from '../components/ClubInput'
 import { editClub, setClubFormDataForEdit } from '../actions/editClub'
 

950be78088c85b525a72eece088651a481e18e62
SallyZ12 <szasloff@gmail.com>
2019-12-21 18:05:07 -0500

    working on edit club function, added variables to render, not working yet

--- src/components/ClubInput.js
+++ src/components/ClubInput.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { addClub } from '../actions/addClub'
+import { updateClubForm } from '../actions/editClub'
 
 class ClubInput extends React.Component {
 
@@ -9,42 +9,61 @@ class ClubInput extends React.Component {
     clubName: '',
     location: ''
   }
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    this.props.addClub(this.state)
-    this.props.history.push('/')
-    this.setState({
-      clubName:'',
-      location: ''
-    })
-  }
+
+  // handleChange = (event) => {
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   })
+  // }
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault()
+  //   this.props.addClub(this.state)
+  //   this.props.history.push('/')
+  //   this.setState({
+  //     clubName:'',
+  //     location: ''
+  //   })
+  // }
 
   render(){
 
-    const {editMode} = this.props
+    const clubFormData = (this.state.clubName, this.state.location)
+
+    const handleClubInfoInputChange = event => {
+      const { name, value } = event.target
+
+    const updatedClubFormInfo = {
+      ...clubFormData,
+      [name]: value
+    }
+    updateClubForm(updatedClubFormInfo)
+  }
+
+    const {editMode} = this.state
+    const {handleSubmit} = this.state
 
     return (
       <div>
-          <h5> New Club/Edit Club </h5>
-        <form onSubmit={this.handleSubmit}>
+
+        <form onSubmit={event => {
+          event.preventDefault()
+          handleSubmit(clubFormData)
+        }}>
+        <h5> New Club/Edit Club </h5>
         <label> Club Name </label>
         <input type='text'
                 placeholder='Name'
                 value={this.state.clubName}
                 name = 'clubName'
-                onChange={this.handleChange}/><br/>
+                onChange={handleClubInfoInputChange}/><br/>
         <label> Location </label>
         <input type='text'
                 placeholder='location'
                 value={this.state.location}
                 name = 'location'
-                onChange={this.handleChange}/><br/>
+                onChange={handleClubInfoInputChange}/><br/>
 
         <input type = "submit"  value={editMode ?   "Update Club Info" : "New Club" }/>
         </form>
@@ -53,7 +72,7 @@ class ClubInput extends React.Component {
   }
 }
 
-export default withRouter(connect(null, {addClub})(ClubInput))
+export default withRouter(connect(null, {updateClubForm})(ClubInput))
 
   // <input type = "submit"  value= "New Club"/>
 // <input type = "submit"  value={editMode ? "Update Club Info" : "New Club"}/>

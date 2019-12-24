b916885beafbb9d7d4229192a37dadcc76687778
SallyZ12 <szasloff@gmail.com>
2019-12-24 14:50:58 -0500

    added route for NewClubForm Wrapper

--- src/containers/ClubsContainer.js
+++ src/containers/ClubsContainer.js
@@ -4,6 +4,7 @@ import {fetchClubs} from '../actions/fetchClubs'
 import ClubInput from '../components/ClubInput'
 import Club from '../components/Club'
 import EditClubFormWrapper from '../components/EditClubFormWrapper'
+import NewClubFormWrapper from '../components/NewClubFormWrapper'
 import CourtInput from '../components/CourtInput'
 import { Route, Switch } from 'react-router-dom'
 
@@ -20,7 +21,9 @@ class ClubsContainer extends React.Component {
         <Route exact path="/clubs/:id" render={(routerProps)=> <Club {...routerProps} clubs={this.props.clubs} currentUser={this.props.currentUser}/>}/>
         <Route exact path = '/clubs' render ={() => <ClubInput clubInput = {this.props.club}/>} />
         <Route exact path = '/clubs/:id/courts' component = {CourtInput}/>
+        <Route exact path="/clubs/:id" component = {NewClubFormWrapper} />
         <Route exact path="/clubs/:id/edit" render={(rProps)=> <EditClubFormWrapper {...rProps} currentUser={this.props.currentUser} />}/>
+
       </Switch>
 
       </div>

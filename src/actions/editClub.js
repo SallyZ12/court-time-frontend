7a4f1da7f460845047339ce48252135fe584b32d
SallyZ12 <szasloff@gmail.com>
2019-12-25 15:30:13 -0500

    added comment for async action creator editClub

--- src/actions/editClub.js
+++ src/actions/editClub.js
@@ -1,30 +1,3 @@
-export const editClub = (club, history) => {
-
-  return dispatch => {
-    const clubInfo = {
-      club: club
-      }
-
-    return fetch(`http://localhost:3000/api/v1/clubs/${club.clubId}`, {
-      credentials: "include",
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(clubInfo)
-    })
-     .then(response => response.json())
-     .then(club => {
-       if (club.error) {
-         alert(club.error)
-       } else {
-       dispatch({type: "EDIT_CLUB", club: club})
-       history.push(`/clubs/${club.id}`)
-     }
-   })
-   }
-  }
-
 
   export const setClubFormDataForEdit = club => {
 
@@ -52,3 +25,32 @@ export const editClub = (club, history) => {
         type: "RESET_CLUB_FORM"
       }
     }
+
+
+  // async action creator
+  export const editClub = (club, history) => {
+
+    return dispatch => {
+      const clubInfo = {
+        club: club
+        }
+
+    return fetch(`http://localhost:3000/api/v1/clubs/${club.clubId}`, {
+      credentials: "include",
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify(clubInfo)
+      })
+        .then(response => response.json())
+        .then(club => {
+          if (club.error) {
+           alert(club.error)
+         } else {
+         dispatch({type: "EDIT_CLUB", club: club})
+         history.push(`/clubs/${club.id}`)
+       }
+     })
+     }
+  }

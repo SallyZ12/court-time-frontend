d6bb98b9880766658a3323226172d18291bda765
SallyZ12 <szasloff@gmail.com>
2019-12-28 19:15:12 -0500

    removed editClub action creator from this file and moved to addClub file

--- src/actions/editClub.js
+++ src/actions/editClub.js
@@ -1,6 +1,20 @@
 
-  export const setClubFormDataForEdit = club => {
+export const updateClubForm = (clubFormData) => {
+    return {
+      type: "UPDATE_CLUB_FORM",
+      clubFormData
+    }
+  }
 
+
+  export const resetClubForm = () => {
+    return {
+      type: "RESET_CLUB_FORM"
+    }
+  }
+
+
+  export const setClubFormDataForEdit = club => {
     const clubFormData = {
       id: club.id,
       clubName: club.club_name,
@@ -11,46 +25,7 @@
       type: "SET_CLUB_FORM_DATA_FOR_EDIT",
       clubFormData
     }
-  }
-
-  export const updateClubForm = (clubFormData) => {
-      return {
-        type: "UPDATE_CLUB_FORM",
-        clubFormData
-      }
-    }
-
-    export const resetClubForm = () => {
-      return {
-        type: "RESET_CLUB_FORM"
-      }
-    }
+}
 
 
-  // async action creator
-  export const editClub = (club, history) => {
-
-    return dispatch => {
-      const clubInfo = {
-        club: club
-        }
-
-    return fetch(`http://localhost:3000/api/v1/clubs/${club.clubId}`, {
-      credentials: "include",
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json"
-        },
-        body: JSON.stringify(clubInfo)
-      })
-        .then(response => response.json())
-        .then(club => {
-          if (club.error) {
-           alert(club.error)
-         } else {
-         dispatch({type: "EDIT_CLUB", club: club})
-         history.push(`/clubs/${club.id}`)
-       }
-     })
-     }
-  }
+  

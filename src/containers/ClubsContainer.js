fe36b27eda41b4f02758e8fd4d35cd6b758b03d1
SallyZ12 <szasloff@gmail.com>
2020-01-04 13:36:57 -0500

    deleting commented out ClubInput component and route since ClubInput imported by NewClubForm Wrapper and EditClubFormWrapper

--- src/containers/ClubsContainer.js
+++ src/containers/ClubsContainer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchClubs} from '../actions/fetchClubs'
-// import ClubInput from '../components/ClubInput'
 import Club from '../components/Club'
 import NewClubFormWrapper from '../components/NewClubFormWrapper'
 import EditClubFormWrapper from '../components/EditClubFormWrapper'
@@ -20,11 +19,12 @@ class ClubsContainer extends React.Component {
     return (
       <div>
       <Switch>
-        {/*<Route exact path = '/clubs' render ={() => <ClubInput clubInput = {this.props.club}/>*/}
+
         <Route exact path="/clubs/:id" render={(routerProps)=> <Club {...routerProps} clubs={this.props.clubs} currentUser={this.props.currentUser}/>}/>
         <Route exact path='/clubs' component = {NewClubFormWrapper} />
         <Route exact path = '/clubs/:id/courts' component = {CourtInput}/>
         <Route exact path="/clubs/:id/edit" render={(rProps)=> <EditClubFormWrapper {...rProps} clubs={this.props.clubs} />}/>
+        
       </Switch>
 
       </div>

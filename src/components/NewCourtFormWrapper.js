3d1ed82bf8f7f765610909db077d4994131594ad
SallyZ12 <szasloff@gmail.com>
2020-01-17 14:23:49 -0500

    new file to start edit court function, starting with new court first

---
+++ src/components/NewCourtFormWrapper.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import CourtInput from '../components/CourtInput'
+import { addCourt } from '../actions/addCourt'
+import { connect } from 'react-redux'
+
+const NewCourtFormWrapper = ( {history, addCourt }) => {
+
+  const handleSubmit = (courtData, clubId) => {
+    // console.log("clubData", clubData)
+      addCourt({
+       ...courtData,
+       clubId
+    }, history)
+  }
+
+  return <CourtInput history= {history} handleSubmit={handleSubmit} />
+}
+
+export default connect(null,  { addCourt })(NewCourtFormWrapper)

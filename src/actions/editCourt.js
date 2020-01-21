50218377451faecf3b0916378fdd98881217c1d6
SallyZ12 <szasloff@gmail.com>
2020-01-21 13:56:26 -0500

    new file for edit court action creators updateCourtForm, resetCourtForm, setCourtFormForData

---
+++ src/actions/editCourt.js
@@ -0,0 +1,31 @@
+
+export const updateCourtForm = (courtFormData) => {
+    return {
+      type: "UPDATE_COURT_FORM",
+      courtFormData
+    }
+  }
+
+
+  export const resetCourtForm = () => {
+    return {
+      type: "RESET_COURT_FORM"
+    }
+  }
+
+
+  export const setCourtFormDataForEdit = court => {
+    const courtFormData = {
+      id: court.id,
+      club_id: court.club_id
+      courtNumber: court.court_number,
+      surface: court.surface,
+      prime: court.prime,
+      non_prime: court.nonPrime
+  }
+
+    return {
+      type: "SET_COURT_FORM_DATA_FOR_EDIT",
+      courtFormData
+    }
+}

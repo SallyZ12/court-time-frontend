5c53e374e26ec3126c7decf5d231ea413b24f7ec
SallyZ12 <szasloff@gmail.com>
2020-01-26 15:32:22 -0500

    working on conversion to global vs local state so can edit court info-- work in progress

--- src/components/CourtInput.js
+++ src/components/CourtInput.js
@@ -2,46 +2,47 @@ import React from 'react'
 import{connect} from 'react-redux'
 import {updateCourtForm} from '../actions/editCourt'
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
+// import Form from 'react-bootstrap/Form';
+// import Col from 'react-bootstrap/Col';
 import { withRouter } from 'react-router-dom';
 
-const CourtInput = ({ courtFormData, updateCourtForm, history, editMode, handleSubmit}) => {
+const CourtInput = ({ courtFormData, updateCourtForm, history, match, params, id, editMode, handleSubmit}) => {
 
   const {courtNumber, surface, prime, nonPrime} = courtFormData
-// console.log("courtFormdData", courtFormData)
+  let clubId = match.params.id
+console.log("courtFormData", courtFormData)
 
   const handleCourtInfoInputChange = event => {
     const { name, value } = event.target
+
     const updatedCourtFormInfo = {
     ...courtFormData,
     [name]: value
   }
 
   updateCourtForm(updatedCourtFormInfo)
+
 }
 
     return (
-      <div className = "Input">
-
-      <Form onSubmit={event=> {
+      <form onSubmit={event => {
         event.preventDefault()
         handleSubmit(courtFormData)
       }}>
 
-        <h5> Court Information </h5>
+        <h5> Court Data </h5>
 
-          <Form.Row>
-            <Form.Group as={Col}>
+
+            <label> Court Number </label>
               <input type= 'integer'
                 placeholder = "Court #"
                 value = {courtNumber}
                 name = 'courtNumber'
                 onChange={handleCourtInfoInputChange}/>
-          </Form.Group>
+            <br/>
 
-        <Form.Group as={Col} >
-        <Form.Label>Surface</Form.Label>
+
+           <label>Surface</label>
               <select
                name = 'surface'
                value = {surface}
@@ -51,33 +52,35 @@ const CourtInput = ({ courtFormData, updateCourtForm, history, editMode, handleS
                <option value = "Har-Tru">Har-Tru</option>
                <option value = "Grass">Grass</option>
             </select>
-            <Button variant="primary"><input type="submit" value="Submit"/></Button>
-        </Form.Group>
-        </Form.Row>
+            <br/>
 
-        <Form.Row>
-        <Form.Group as={Col} >
-
-          <input type= 'integer'
+            <label> Prime Rate </label>
+             <input type= 'integer'
                 placeholder = "Prime Rate"
                 value = {prime}
                 name = 'prime'
                 onChange={handleCourtInfoInputChange}/>
+            <br/>
 
-          <input type= 'integer'
+            <label> Non-Prime Rate </label>
+              <input type= 'integer'
                   placeholder = "Non-Prime Rate"
                   value = {nonPrime}
                   name = 'nonPrime'
                   onChange={handleCourtInfoInputChange}/><br/>
-        </Form.Group>
-        </Form.Row>
 
-        </Form>
+        <Button variant="primary"><input type="submit" value={editMode ? "Update Court Info" : "New Court"}/></Button>
+        </form>
 
-        </div>
-    )
+      )
   }
 
+const mapStateToProps = state =>{
+
+  return{
+    courtFormData: state.courtFormReducer
+  }
+}
 
 
-export default withRouter(connect(null, {updateCourtForm})(CourtInput))
+export default withRouter(connect(mapStateToProps, {updateCourtForm})(CourtInput))

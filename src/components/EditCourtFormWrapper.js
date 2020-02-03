8abe2e51c70647b150e101e7107c03ab4db8f5e2
SallyZ12 <szasloff@gmail.com>
2020-02-03 16:38:30 -0500

    started working on Class Function for Editing Court info

---
+++ src/components/EditCourtFormWrapper.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import CourtInput from '../components/CourtInput'
+import { setCourtFormDataForEdit, resetCourtForm } from '../actions/editCourt'
+import { editCourt } from '../actions/addCourt'
+import {connect} from 'react-redux'
+
+class EditCourtFormWrapper extends React.Component {
+
+
+  componentDidMount() {
+    this.props.clubs.clubs && this.props.setCourtFormDataForEdit(this.props.clubs.clubs.filter(club => club.id === parseInt(this.props.match.params.id))[0])
+
+  }
+
+  componentDidUpdate(prevProps) {
+    this.props.clubs.clubs && !prevProps.clubs.clubs && this.props.setCourtFormDataForEdit(this.props.clubs.clubs.filter(club => club.id === parseInt(this.props.match.params.id))[0])
+  }
+
+  componentWillUnmount(){
+    this.props.resetCourtForm()
+  }
+
+  handleSubmit = (courtFormData) => {
+     const { editCourt, clubNumber = parseInt(this.props.match.params.id), history } = this.props
+
+      editCourt({
+        ...courtFormData,
+        clubId: clubNumber
+      }, history)
+  }
+
+  render() {
+    return <>
+      <CourtInput editMode handleSubmit={this.handleSubmit} />
+      <br/>
+      </>
+    }
+}
+
+export default connect(null, {editCourt, setCourtFormDataForEdit, resetCourtForm})(EditCourtFormWrapper)
